99dd96d37644ec1d43483a4d8b8315f865d8a7ef
Amiry <amirydev@gmail.com>
2018-01-14 15:49:37 +0200

    deployment
    #just for fun

--- src/components/HighScores.js
+++ src/components/HighScores.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { MD5 as md5 } from 'object-hash';
 
 const HighScores = props => {
-    const scores = props.scores.map(score => {
-        const hashKey = md5(score);
+    const sortedScores = props.scores
+        .sort((a, b) => (a.score > b.score));
+    const scores = sortedScores.map((score, i) => {
         return (
-            <tr key={hashKey}>
+            <tr key={i}>
                 <td>{score.name}</td>
                 <td>{score.score}</td>
             </tr>
